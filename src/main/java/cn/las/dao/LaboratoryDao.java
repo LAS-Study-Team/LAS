1a94e4220564a81cf15566bd9c3ed77be65976f1
windhunter <2034398174@qq.com>
2020-10-19 15:18:25 +0800

    部署在服务器系统0.0.1

--- src/main/java/cn/las/dao/LaboratoryDao.java
+++ src/main/java/cn/las/dao/LaboratoryDao.java
@@ -2,7 +2,6 @@ package cn.las.dao;
 
 import cn.las.domain.Laboratory;
 import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
